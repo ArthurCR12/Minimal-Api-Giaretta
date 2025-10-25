bf134c2d091374e76a2d643e039afd1f91bf03ce
ArthurCabral <pacrbr60@gmail.com>
2025-10-25 12:11:21 -0300

    feat: Person Model

---
+++ Models/PersonModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Person.Models;
+
+public class PersonModel
+{
+    public PersonModel(string name)
+    {
+        Name = name;
+        Id = Guid.NewGuid();
+    }
+
+    // Esse init permite que a propriedade seja definida apenas durante a inicialização do objeto
+    public Guid Id { get; init; }
+    public string Name { get; private set; }
+}
