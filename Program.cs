a26203af25f4fee409354acf340081f97e43e0f4
ArthurCabral <pacrbr60@gmail.com>
2025-10-27 11:14:27 -0300

    Banco dados

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Person.Data;
 using Person.Routes;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddScoped<PersonContext>();
 
 var app = builder.Build();
 
