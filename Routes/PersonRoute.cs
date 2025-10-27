d0b5de360d9d5b72b2dfcdcb8d7b75144983f80f
ArthurCabral <pacrbr60@gmail.com>
2025-10-27 11:14:40 -0300

    feat: CRUD nas rotas

--- Routes/PersonRoute.cs
+++ Routes/PersonRoute.cs
@@ -1,4 +1,6 @@
-﻿using Person.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using Person.Data;
+using Person.Models;
 
 namespace Person.Routes;
 
@@ -6,6 +8,41 @@ public static class PersonRoute
 {
     public static void PersonRoutes(this WebApplication app)
     {
-        app.MapGet("person", () => new PersonModel("Arthur"));
+        var route = app.MapGroup("person");
+        route.MapGet("", async (PersonContext context) =>
+        {
+            var people = await context.People.ToListAsync();
+            return Results.Ok(people);
+        });
+
+        route.MapPost("", async (PersonRequest req, PersonContext context) =>
+        {
+            var person = new PersonModel(req.name);
+            await context.AddAsync(person);
+            await context.SaveChangesAsync();
+        });
+
+        route.MapPut("{id:guid}", async (Guid id, PersonRequest req, PersonContext context) =>
+        {
+            var person = await context.People.FirstOrDefaultAsync(x => x.Id == id);
+            if (person == null)
+                return Results.NotFound();
+
+            person.ChangeName(req.name);
+            await context.SaveChangesAsync();
+            return Results.Ok(person);
+        });
+
+        route.MapDelete("{id:guid}", async (Guid id, PersonContext context) =>
+        {
+            // soft delete, apenas desativa a pessoa
+            var person = await context.People.FirstOrDefaultAsync(x => x.Id == id);
+            if (person == null)
+                return Results.NotFound();
+
+            person.SetInactive();
+            await context.SaveChangesAsync();
+            return Results.Ok(person);
+        });
     }
 }
